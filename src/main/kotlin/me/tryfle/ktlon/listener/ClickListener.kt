e89962a4a0a15b04fe62dfa49c0af322a1ed903e
Tryfle <xinaout410@gmail.com>
2024-01-08 20:00:33 -0500

    :purple_heart: Improvement.

--- src/main/kotlin/me/tryfle/ktlon/listener/ClickListener.kt
+++ src/main/kotlin/me/tryfle/ktlon/listener/ClickListener.kt
@@ -11,12 +11,15 @@ import net.minecraft.client.settings.KeyBinding
 import net.weavemc.loader.api.event.MouseEvent
 import net.weavemc.loader.api.event.SubscribeEvent
 import net.weavemc.loader.api.event.TickEvent
+import org.lwjgl.input.Mouse
 
 class ClickListener {
 
     private var cps: Int = 0
     private var t: TimerUtil = TimerUtil()
+    private var t2: TimerUtil = TimerUtil()
     private var shouldClick: Boolean = false
+    private var shouldReturn: Boolean = false
 
     private fun getEventButton(): Int {
         return if (Data.loadData().isLmb) 0 else 1
@@ -31,11 +34,9 @@ class ClickListener {
         val sleepTime: Long = Util.randInt(30, 60).toLong()
         if (shouldClick) {
             delay(sleepTime)
-            KeyBinding.setKeyBindState(getButton(), false)
-            KeyBinding.onTick(getButton())
-            delay(sleepTime + Util.randInt(-10, 10).toLong())
             KeyBinding.setKeyBindState(getButton(), true)
             KeyBinding.onTick(getButton())
+            delay(sleepTime + Util.randInt(-10, 10).toLong())
             KeyBinding.setKeyBindState(getButton(), false)
             KeyBinding.onTick(getButton())
             shouldClick = false
@@ -44,16 +45,16 @@ class ClickListener {
 
     @SubscribeEvent
     fun onClick(e: MouseEvent) {
-        if (Data.instance.enabled && Minecraft.getMinecraft().thePlayer != null && e.button == getEventButton()) {
+        if (Data.instance.enabled && Minecraft.getMinecraft().thePlayer != null && e.button == getEventButton() && Mouse.isButtonDown(getEventButton()) && !shouldReturn) {
+            println("[K-Debug] MouseEvent fired, condition met.")
             cps++
             if (shouldClick) {
                 val ch: Double = Math.random() * 100;
-                if (ch.toInt() >= Data.instance.chance) {
-                    return
-                }
+                if (ch.toInt() >= Data.instance.chance) return
+                if (Data.instance.fiveCps && cps < 7) return //for some reason 5 cps is 7 cps lol??
             }
-            if (Data.instance.fiveCps && cps < 7) return //for some reason 5 cps is 7 cps lol??
             GlobalScope.launch { rClick() }
+            shouldReturn = true // exists in order to prevent the mouse event from firing twice, which it LOVES doing
         }
     }
 
@@ -62,6 +63,10 @@ class ClickListener {
         if (t.hasPassed(1000)) {
             cps = 0
             t.reset()
+        }
+        if (t2.hasPassed(10)) {
+            shouldReturn = false
+            t2.reset()
         }
     }
 }
